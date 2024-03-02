5eadfb9d1875e31f00e16234e73fe77a76ad8c65
Gavin Nitta <gshigeto@gmail.com>
2024-03-01 22:00:59 -0700

    feat: Make app PWA

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,16 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vitest/config";
+import { VitePWA } from "vite-plugin-pwa";
 import viteTsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   // depending on your application, base can also be "/"
   base: "",
-  plugins: [react(), viteTsconfigPaths()],
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+    VitePWA({ registerType: "autoUpdate" }),
+  ],
   server: {
     // this sets a default port to 3000
     host: true,
